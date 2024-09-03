0c8d1928ae2066cf3430bd8cc65659c404fbf310
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-09-02 20:23:34 -0700

    h

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -155,12 +155,12 @@ function ReviewGrid() {
 export function Reviews() {
   return (
     <MaxWidthWrapper className='relative max-w-5xl'>
-      <img
+      {/* <img
         aria-hidden='true'
         alt='img'
         src='/what-people-are-buying.png'
         className='absolute select-none hidden xl:block -left-32 top-1/3'
-      />
+      /> */}
       <img src='/snake-1.png' alt='img' className='w-full' />
 
       <ReviewGrid />
