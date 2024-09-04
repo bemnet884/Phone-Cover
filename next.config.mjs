31d1a9ddecb3f78466f2a1ae0486a6be6b761e74
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-09-04 15:09:30 -0700

    create

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export"
-  
-};
+  images: {
+    domains: ['utfs.io'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
